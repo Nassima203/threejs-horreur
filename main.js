ba6ed96c01043dc41f0efd43ed85fdf9657bb781
c-txr <chaden.teixeira78@gmail.com>
2026-05-06 16:12:37 +0200

    fix: précision des réponses correctes pour chaque film

--- main.js
+++ main.js
@@ -57,22 +57,22 @@ const catalogueFilms = {
         trivia: "Le film est si extrême qu'à sa sortie en France, la commission de classification a d'abord voulu l'interdire aux moins de 18 ans avec avertissement, une rareté qui aurait tué sa carrière commerciale."
     },
     "masque - scream": { 
-        answers: ["scream", "ghostface"], 
+        answers: ["scream"], 
         id: "4232", // Pointe directement sur le film de 1996
         trivia: "Le masque de Ghostface n'a pas été créé pour le film. Il a été découvert par hasard par la productrice Marianne Maddalena dans un magasin de farces et attrapes lors de repérages."
     },
     "masque - vendredi 13": { 
-        answers: ["vendredi 13", "friday the 13th", "jason voorhees"], 
+        answers: ["vendredi 13", "friday the 13th"], 
         id: "4488",
         trivia: "L'actrice Betsy Palmer a accepté le rôle de la mère de Jason uniquement parce qu'elle avait besoin d'argent pour s'acheter une nouvelle voiture. Elle pensait que personne ne verrait ce film."
     },
     "masque - le silence des agneaux": { 
-        answers: ["le silence des agneaux", "the silence of the lambs", "hannibal lecter"], 
+        answers: ["le silence des agneaux", "the silence of the lambs"], 
         id: "274",
         trivia: "Anthony Hopkins ne cligne pratiquement jamais des yeux durant tout le film, une technique qu'il a apprise en observant des vidéos de reptiles pour rendre Hannibal Lecter plus inhumain."
     },
     "chapeau krugger": { 
-        answers: ["freddy", "les griffes de la nuit", "a nightmare on elm street"], 
+        answers: ["freddy les griffes de la nuit", "les griffes de la nuit", "a nightmare on elm street"], 
         id: "377",
         trivia: "Wes Craven a eu l'idée de Freddy en lisant des articles de presse sur des réfugiés cambodgiens décédés en plein cauchemar, un phénomène médical réel appelé syndrome de la mort subite inexpliquée."
     },
@@ -82,12 +82,12 @@ const catalogueFilms = {
         trivia: "Le personnage de Mick Taylor est largement inspiré d'Ivan Milat, le tristement célèbre tueur de routards qui a sévi en Australie dans les années 90."
     },
     "cassette the ring": { 
-        answers: ["the ring", "le cercle", "samara"], 
+        answers: ["the ring", "le cercle"], 
         id: "565",
         trivia: "Lors de la projection test, certains spectateurs étaient tellement terrifiés par la scène de la TV qu'ils ont quitté la salle en étant persuadés que leur propre téléviseur à la maison allait les maudire."
     },
     "ballon - it": { 
-        answers: ["it", "ça", "grippe-sou", "pennywise"], 
+        answers: ["it", "ça"], 
         id: "346364",
         trivia: "Pour terroriser les enfants acteurs, Bill Skarsgård a été gardé à l'écart du reste du casting jusqu'à sa première scène. Les pleurs et les cris des enfants à l'écran sont souvent de vraies réactions de peur."
     },
@@ -97,7 +97,7 @@ const catalogueFilms = {
         trivia: "Scott Cawthon, le créateur, a eu l'idée du jeu après que les critiques ont qualifié les personnages de son précédent jeu trop effrayants et ressemblant à des automates défectueux."
     },
     "skate": { 
-        answers: ["a girl walks home alone at night", "bad city"], 
+        answers: ["a girl walks home alone at night"], 
         id: "252171",
         trivia: "Le skate appartenait personnellement à la réalisatrice Ana Lily Amirpour. L'actrice Sheila Vand a dû s'entraîner pour apprendre à glisser en portant son tchador, symbolisant la liberté de son personnage."
     },
@@ -107,7 +107,7 @@ const catalogueFilms = {
         trivia: "Pour obtenir une peur authentique, les actrices n'ont jamais vu les Crawlers avant le premier jour de tournage de leur rencontre. La panique lors de la première attaque est réelle."
     },
     "chaise -conjuring": { 
-        answers: ["the conjuring", "les dossiers warren"], 
+        answers: ["the conjuring", "conjuring"], 
         id: "138843", 
         trivia: "La véritable famille Perron a visité le plateau, mais un vent violent s'est levé soudainement, renversant tout le matériel, alors que les arbres à quelques mètres restaient immobiles."
     },
@@ -117,12 +117,12 @@ const catalogueFilms = {
         trivia: "Ce film est le grand-père du slasher moderne. John Carpenter a avoué que le masque blanc inexpressif de Michael Myers dans Halloween est directement inspiré de ce film français."
     },
     "robe de chambre exorciste": { 
-        answers: ["lexorciste", "the exorcist", "regan"], 
+        answers: ["lexorciste", "the exorcist"], 
         id: "9552",
         trivia: "Le plateau de tournage de la maison a entièrement brûlé dans un incendie mystérieux, sauf la chambre de Regan, qui est restée intacte alors que tout le reste était en cendres."
     },
     "masque micheal myers": { 
-        answers: ["halloween", "michael myers"], 
+        answers: ["halloween"], 
         id: "948",
         trivia: "Faute de budget, l'équipe a acheté un masque du capitaine Kirk (Star Trek) pour quelques dollars, l'a peint en blanc et a élargi les yeux pour créer le visage de Michael Myers."
     },
@@ -137,7 +137,7 @@ const catalogueFilms = {
         trivia: "Pour la célèbre scène de la douche, Alfred Hitchcock a utilisé du sirop de chocolat pour simuler le sang, car sa densité passait mieux en noir et blanc que les colorants classiques."
     },
     "noeud_papillon_saw": { 
-        answers: ["saw", "jigsaw"], 
+        answers: ["saw"], 
         id: "176",
         trivia: "Le budget était si serré que James Wan a fabriqué lui-même la marionnette Billy à partir de vieux journaux, d'essuie-tout et de balles de ping-pong pour les yeux."
     },
@@ -147,7 +147,7 @@ const catalogueFilms = {
         trivia: "Le tournage a eu lieu dans une véritable attraction hantée. L'équipe raconte que certains mannequins changeaient de place d'eux-mêmes entre les prises."
     },
     "mitaines_terrifier": { 
-        answers: ["terrifier", "art the clown"], 
+        answers: ["terrifier"], 
         id: "420634",
         trivia: "David Howard Thornton, qui joue Art le Clown, a une formation de mime. Il a décroché le rôle en improvisant une décapitation silencieuse avec un plaisir immense."
     },
@@ -322,7 +322,7 @@ frontWall.position.set(0, 2.5, 5);
 frontWall.receiveShadow = true;
 scene.add(frontWall);
 
-// Plafond (Applique le matériau texturé directement ici)
+// Plafond 
 const ceiling = new THREE.Mesh(new THREE.PlaneGeometry(10, 10), ceilingMat);
 ceiling.position.y = 5;
 ceiling.rotation.x = Math.PI / 2;
@@ -690,6 +690,7 @@ if (btnNon) {
     };
 }
 
+
 document.getElementById('confirm-ouija').onclick = async () => {
     const filmData = catalogueFilms[objetSurvoleNom];
     const match = filmData.answers.some(a => a.replace(/\s/g, '').toLowerCase() === tentativeActuelle.replace(/\s/g, '').toLowerCase());
@@ -740,9 +741,7 @@ function updateSanityUI() {
         // Changement de couleur selon l'état critique
         if (santeMentale <= 30) {
             fill.style.background = "linear-gradient(90deg, #500, #b00)"; // Rouge sang
-        } else {
-            fill.style.background = "linear-gradient(90deg, #222, #444)"; // Gris sombre
-        }
+        } 
     }
 }
 
